c42e4fa58431af3981da7f5fc4adaa77d0f066fe
Oskar Lindenau <oskarlindenau@gmail.com>
2025-11-13 21:48:26 +0100

    add proxy rule

--- frontend/src/online-chat-ui/vite.config.ts
+++ frontend/src/online-chat-ui/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5137,
+    proxy: {
+      // All requests starting with /api go to backend
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
+
